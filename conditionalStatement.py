afe6d0d0faeea4828220539d364fda7666cb82b2
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-24 08:12:07 +0600

    Conditional Logic

---
+++ conditionalStatement.py
@@ -0,0 +1,10 @@
+x = 10;
+
+if x > 5:
+    print("Greater than five")
+if x > 7:
+    print("Greater than seven")
+elif x > 8:
+    print("Greater than eight")
+else:
+    print("Else executed")
--- firstCode.py
+++ firstCode.py
@@ -1,1 +1,3 @@
 print("Yo world")
+a= 2 ** 3 + 'python' *2 + 5 // 2
+print(a)
