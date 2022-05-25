26d995bf58dc25558fcb61adcfc75f7855cdce00
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-25 08:44:12 +0600

    A few programs with functions

--- progsWithFunction.py
+++ progsWithFunction.py
@@ -14,6 +14,32 @@ def fibo(n):
             x=y
             y=z
             print(z, end=" ")
-
-
 fibo(4)
+
+
+#Age guessing game
+
+import random as rn
+
+secretAge = rn.randint(1,10)
+flag = False
+
+def age_game(guessedAge, secret):
+    if guessedAge<secret:
+        return "Too low"
+    elif guessedAge>secret:
+        return "Too high"
+    else:
+        return "CORRECT!"
+
+for i in range (1,6):
+    print('Take a guess. You have ', 6-i, 'guesses left: ')
+    guess= int(input())
+    if age_game(guess, secretAge) == 'CORRECT!':
+        print('You WON the GAME')
+        flag= True
+        break
+if not flag:
+    print("You LOST the GAME")
+
+
