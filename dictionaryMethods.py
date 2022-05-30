395beb1dd399b83d881268204ef99999840397a7
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-30 10:42:18 +0600

    Dict methods

--- dictionaryMethods.py
+++ dictionaryMethods.py
@@ -16,4 +16,38 @@ for i in identity.keys():
 for i in identity.items():
     print(i)
 
+#a handy trick to print both under a loop
+for k,v in identity.items():
+    print(k,v)
+
+#convert dict values and keys into a list
+
+x= list(identity.keys())
+y= list(identity.values())
+
+print(x,y)
+
+#in keyboard
+print('name' in identity)
+print('Polok' in identity)
+
+#in keyboard - in values
+print('name' in identity.values())
+print('Polok' in identity.values())
+
+#in keyboard - in keys
+print('name' in identity.keys())
+print('Polok' in identity.keys())
+
+#the get() method
+print(str(identity.get('name', 'not found')))
+print(str(identity.get('height', 'not found')))
+
+#the setdefault() Method
+print(str(identity.setdefault('name', 'Ferdaus')))
+print(str(identity.setdefault('height', '5ft 10in')))
+
+print(identity)
+
+
 
