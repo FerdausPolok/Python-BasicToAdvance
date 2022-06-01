7bdbd00f45e56f7cda2aaf168740c902861ec084
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-06-01 09:25:51 +0600

    update

--- OOP/StringFormating.py
+++ OOP/StringFormating.py
@@ -2,7 +2,7 @@ name= 'Polok'
 age= 25
 height= 5.9
 
-print("I am {}, I am {} years old and my height is {} ft".format(name, age, height))
+print("I am {}, I am {} years old and my eight is {} ft".format(name, age, height))
 print("I am {1}, I am {0} years old and my height is {2} ft".format(age, name, height))
 
-print(f"My name is {name}, I am {age} years old and my height is {height} ft")
+print(f"My name is {name}, I\'m {age} years old and my height is {height} ft")
