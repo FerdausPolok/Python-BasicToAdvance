485d2e82efdf30199a5196a4ad409b36da88f424
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-06-01 09:20:01 +0600

    Python String Fromatting Basics

---
+++ OOP/StringFormating.py
@@ -0,0 +1,8 @@
+name= 'Polok'
+age= 25
+height= 5.9
+
+print("I am {}, I am {} years old and my height is {} ft".format(name, age, height))
+print("I am {1}, I am {0} years old and my height is {2} ft".format(age, name, height))
+
+print(f"My name is {name}, I am {age} years old and my height is {height} ft")
