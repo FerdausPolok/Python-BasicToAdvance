ce195c7ced164be971c0305c898fdea6f6acadda
Ferdaus Zaman Polok <68804751+FerdausPolok@users.noreply.github.com>
2022-06-04 13:16:56 +0600

    Python Magic Method – Part 2

--- OOP/magic_method.py
+++ OOP/magic_method.py
@@ -10,6 +10,25 @@ class Point2D:
     def __str__(self): #general purpose to know about object/ instance
         return f"Class: Point2D, " \
                f"X: {self.x}, y: {self.y}"
+    def __add__(self, other):
+        if isinstance(self, other.__class__):
+            return Point2D(self.x + other.x, self.y + other.y)
+        else:
+            return None
+    def __eq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.x == other.x and self.y == other.y
+    def __nq__(self, other):
+        if isinstance(self, other.__class__):
+            return self.x != other.x or self.y != other.y
+    def __lt__(self, other):
+        if isinstance(self, other.__class__):
+            return self.x < other.x or (self.y == other.x and self.y< other.y)
+    def __hash__(self):
+        pass
+
+
+
 
 
 p1 = Point2D(1, 2)
@@ -24,3 +43,45 @@ print(repr(p2))
 print(str(p1))
 
 #__ <- these are called danders
+
+print("\n")
+print("More Magic methods... \n\n")
+
+print(f"Sum: {1+1}")
+print(f"Sum: {int.__add__(1,1)}")
+
+"""
+print(f"Object sum: {p1 + p2}") 
+will trow type error
+to solve this we need to orver write the __add__ 
+"""
+
+print(f"Object sum: {p1 + p2}")
+#same goes for subtract
+
+print("\neq... \n")
+
+print(p1 == p1 ) #first a none, overwrite korar por false
+print(p1 is p1)
+
+p6 = Point2D(1, 2)
+
+print(p1 == p6 )
+print(p1 is p6)
+
+print("\nnot eq... \n")
+
+print(p1 != p2 )
+print(p1 != p6 )
+
+print("\nless tan... \n")
+
+print(p1 < p2 )
+print(p1 < p6 )
+
+print("\nhash... \n")
+
+
+
+
+
